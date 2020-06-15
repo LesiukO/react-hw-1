e50dcbc02fa73cebcd4f88e81c7ff33b5efb89fe
LesiukO <lesuksasha@gmail.com>
2020-06-15 19:43:32 +0300

    Task-6-7-8

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Moment from 'react-moment';
+import 'moment-timezone';
+
 const listOfPresidents = [
   'Andrew Jackson',
   'Martin Van Buren',
@@ -42,6 +45,30 @@ const formatPresident = president => {
   return `${president.lastName}, ${president.firstName}, ${president.presidentIndex}-й`;
 }
 
+const data = [{"id":"666958530825467","title":"Friday open-air party w/ Lucarelli, Mihas and Yarik JR | Hide","place":"Hide","date":"2020-06-12T20:00:00.000Z"},{"id":"786185895252176","title":"Захист скверу імені Чкалова","place":"Сквер Им. Чкалова","date":"2020-06-10T09:00:00.000Z"},{"id":"623921328209118","title":"Живая музыка на летней террасе","place":"От Заката до Рассвета","date":"2020-06-14T17:00:00.000Z"},{"id":"909559356190295","title":"Amer (2009)","place":"Кіноклуб Кіноха","date":"2020-06-13T15:00:00.000Z"},{"id":"589272605321022","title":"В парк Межигорье на теплоходе","place":"Причал №6, Почтовая пл.","date":"2020-06-13T07:45:00.000Z"}];
+
+
+const getTimesOfDay = date => {
+  let hour = new Date(date).getHours();
+  
+  if (hour >= 21 || hour < 5) {
+    return 'Ночь';
+  } else if (hour >= 5 && hour < 11) {
+    return 'Утро';
+  } else if (hour >= 11 && hour < 17) {
+    return 'День';
+  } else if (hour >= 17 && hour < 21) {
+    return 'Вечер';
+  }
+
+}
+
+const isInPast = date => {
+  if (new Date(date) < new Date()) {
+    return true;
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <ul>
@@ -56,7 +83,7 @@ ReactDOM.render(
       <li>John Quincy Adams</li>
     </ol>
 
-    <ul>
+    <ul style={{background: '#ddd', paddingTop: '1em', paddingBottom: '1em', fontWeight: 'bold'}}>
       {listOfPresidents.map(president => {
         return <li key={president}>{president}</li>
       })}
@@ -67,6 +94,20 @@ ReactDOM.render(
         return president.presidentIndex % 2 !== 0;
       }).map(president => {
         return <li key={president.presidentIndex}>{formatPresident(president)}</li>
+      })}
+    </ul>
+
+    <ul>
+      {data.sort((previous, next) => {
+        if (previous.date < next.date) {
+          return -1;
+        }
+      }).map( event => {
+        return <li key={event.id} className={isInPast(event.date) && 'past'}>
+          <p><a target="_blank" href={`https://www.facebook.com/events/${event.id}/`}>{event.title}</a></p>
+          <p><span>{getTimesOfDay(event.date)},</span> <Moment format="DD/MM/YYYY, HH:mm:ss">{event.date}</Moment></p>
+          <p>{event.place}</p>
+        </li>
       })}
     </ul>
 
